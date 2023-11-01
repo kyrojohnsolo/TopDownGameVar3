6f2bf9a0f831697a878be5cd3b6e42da9577d964
kyrojohnsolo <146120321+kyrojohnsolo@users.noreply.github.com>
2023-11-01 13:02:41 -0400

    Completed Top Down Game
    Will do bonus lessons next. See if there's anything from previous lesson that could be used, maybe crosshair?

--- TopDownGame-Var3/main.lua
+++ TopDownGame-Var3/main.lua
@@ -1,6 +1,7 @@
---Currently on lesson 41
+--Lesson Complete!
 
 function love.load() -- this function loads everything when game starts
+    math.randomseed(os.time())
     sprites = {} -- creates table for sprites to be stored
     sprites.background = love.graphics.newImage('sprites/background.png') -- adds the background sprite
     sprites.bullet = love.graphics.newImage('sprites/bullet.png') -- adds the bullet sprite
@@ -12,28 +13,38 @@ function love.load() -- this function loads everything when game starts
     player.y = love.graphics.getHeight() / 2 -- assigns the starting player y position to the screen height (center)
     player.speed = 180 -- creates initial character speed value.
     
+    myFont = love.graphics.newFont(30)
+    
     zombies = {} -- create global zombies table. function spawnZombie() creates and adds zombies to this table.
     bullets = {} -- create global bullets table. function spawnBullet() creates and adds zombies to this table.
+
+    gameState = 1 -- gameState 1 is the mainmenu, gameState 2 is when user is playing the game
+    score = 0 -- score is initally set to 0
+    maxTime = 2 -- maxTime is used for zombie timer.
+    timer = maxTime -- timer is used for zombie timer.
 end
 
 function love.update(dt) -- this is the "game loop" that runs at 60FPS
+    if gameState == 2 then
     --[[
         ** PLAYER MOVEMENT **
         The code below handles the player movement using the love.keyboard.isDown() function
         To compensate for framerate, player.speed is multiplied by dt. So if the framerate changes, the speed will adjust as needed.
+        additional and is added to if statement to prevent user from moving off the screen.
     ]]
-    if love.keyboard.isDown("d") then -- if statement checks if user is pressing "d" (right) and moves character
+    if love.keyboard.isDown("d") and player.x < love.graphics.getWidth() then -- if statement checks if user is pressing "d" (right) and moves character
         player.x = player.x + player.speed*dt
     end
-    if love.keyboard.isDown("a") then -- if statement checks if user is pressing "a" (left) and moves character
+    if love.keyboard.isDown("a") and player.x > 0 then -- if statement checks if user is pressing "a" (left) and moves character
         player.x = player.x - player.speed*dt
     end
-    if love.keyboard.isDown("w") then -- if statement checks if user is pressing "w" (up) and moves character
+    if love.keyboard.isDown("w") and player.y > 0 then -- if statement checks if user is pressing "w" (up) and moves character
         player.y = player.y - player.speed*dt
     end
-    if love.keyboard.isDown("s") then -- if statement checks if user is pressing "s" (down) and moves character
+    if love.keyboard.isDown("s") and player.y < love.graphics.getHeight() then -- if statement checks if user is pressing "s" (down) and moves character
         player.y = player.y + player.speed*dt
     end
+end
 
     --[[
         Documenting Logic
@@ -53,6 +64,9 @@ function love.update(dt) -- this is the "game loop" that runs at 60FPS
         if distanceBetween(z.x, z.y, player.x, player.y) < 30 then
             for i,z in ipairs(zombies) do
                 zombies[i] = nil
+                gameState = 1
+                player.x = love.graphics.getWidth() / 2 
+                player.y = love.graphics.getHeight() / 2
             end
         end
     end
@@ -78,11 +92,73 @@ function love.update(dt) -- this is the "game loop" that runs at 60FPS
             table.remove(bullets, i)
         end
     end
+    --[[
+        **BULLET AND ZOMBIE COLLISION DETECTION**
+        this is a nested loop for zombies and bullets that checks the distances between the zombie and bullets.
+        When the less-than distance is met, the zombie and bullet status is set to "dead"
+    ]]
+    for i,z in ipairs(zombies) do
+        for j,b in ipairs(bullets) do
+            if distanceBetween(z.x, z.y, b.x, b.y) < 20 then
+                z.dead = true
+                b.dead = true
+                score = score + 1
+            end
+        end
+    end
+    --[[
+        **REMOVE DEAD ZOMBIES**
+        The following for loop checks the dead status of all zombies.
+        if dead status is found to be true, the zombie is then removed from the table.
+    ]]
+    for i=#zombies, 1, -1 do
+        local z = zombies[i]
+        if z.dead == true then
+            table.remove(zombies, i)
+        end
+    end
+    --[[
+        **REMOVE DEAD BULLETS**
+        The following for loop checks the dead status of all bullets.
+        if dead status is found to be true, the bullet is then removed from the table.
+    ]]
+    for i=#bullets, 1, -1 do
+        local b = bullets[i]
+        if b.dead == true then
+            table.remove(bullets, i)
+        end
+    end
+    --[[
+        **ZOMBIE SPAWN TIMER**
+        timer counts down to zero and spawns zombie.
+        maxTime is reduced to increase spawn speed
+    ]]
+    if gameState == 2 then
+        timer = timer - dt
+        if timer <= 0 then
+            spawnZombie()
+            maxTime = 0.95 * maxTime
+            timer = maxTime
+        end
+    end
 end
 
 function love.draw() -- this function handles drawing the graphics.
     --https://love2d.org/wiki/love.graphics.draw
     love.graphics.draw(sprites.background, 0, 0) -- draws the background
+
+    --[[
+        during gamestate = 1 , user is prompted to click anywhere to begin game.
+    ]]
+    if gameState == 1 then
+        love.graphics.setFont(myFont)
+        love.graphics.printf("Click anywhere to begin!", 0, 50, love.graphics.getWidth(), "center")
+    end
+    --[[
+        score is printed at the bottom of the screen.
+    ]]
+    love.graphics.printf("score: " .. score, 0, love.graphics.getHeight() -100, love.graphics.getWidth(), "center")
+
     --[[
         Just documenting the logic here.
         love.graphics.draw() draws the player sprite, the position going to the global player.x and player.y value.
@@ -119,10 +195,16 @@ function love.keypressed(key)
 end
 --[[
     this activates the bullets when mouse 1 is clicked.
+    bullets will only fire when gameState = 2
 ]]
 function love.mousepressed(x, y, button)
-    if button == 1 then
+    if button == 1 and gameState == 2 then
         spawnBullet()
+    elseif button == 1 and gameState == 1 then -- this will reset game after player is caught.
+        gameState = 2
+        maxTime = 2
+        timer = maxTime
+        score = 0
     end
 end
 
@@ -150,9 +232,33 @@ end
 ]]
 function spawnZombie()
     local zombie = {} -- creates local zombie variable
-    zombie.x = math.random(0, love.graphics.getWidth()) -- assigns position X to random number on screen
-    zombie.y = math.random(0, love.graphics.getHeight()) -- assigns position Y to random number on screen
+    zombie.x = 0 -- creates x position variable
+    zombie.y = 0 -- creates y position variable
     zombie.speed = 140 -- assigns the zombie speed
+    zombie.dead = false -- when zombie collides with bullet, dead will be set to true and the zombie is removed.
+
+    --[[
+        **RANDOM ZOMBIE PLACEMENT**
+        Goal here is to have zombies walk in from random spot off the screen.
+        side variable gets a random number 1-4 which represents each side of the screen.
+        1 = left, 2 = right, 3 = top, 4 = bottom
+
+    ]]
+    local side = math.random(1,4)
+    if side == 1 then
+        zombie.x = -30
+        zombie.y = math.random(0, love.graphics.getHeight())
+    elseif side == 2 then
+        zombie.x = love.graphics.getWidth() + 30
+        zombie.y = math.random(0, love.graphics.getHeight())
+    elseif side == 3 then
+        zombie.x = math.random(0, love.graphics.getWidth())
+        zombie.y = -30
+    elseif side == 4 then
+        zombie.x = math.random(0, love.graphics.getWidth())
+        zombie.y = love.graphics.getHeight() + 30
+    end
+
     table.insert(zombies, zombie) -- adds the local "zombie" to the global "zombies" table.
 
 end
@@ -166,6 +272,7 @@ function spawnBullet()
     bullet.y = player.y -- assigns position y to player location 
     bullet.speed = 500 -- assings the bullet speed
     bullet.direction = playerMouseAngle() -- uses the player mouse angle for the direction of the bullet will go
+    bullet.dead = false -- when bullets collide with zombies, dead will be set to true and bullet is removed
     table.insert(bullets, bullet) -- inserts bullet into global bullets table.
 end
 
